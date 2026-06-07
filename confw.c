5b0cf762aca8dcd56ee9e22fadd5a231479eca96
uncle_joe <uncle_joe@kali.kali.com>
2026-06-07 10:05:55 +0300

    rectangle's step is configurable

--- confw.c
+++ confw.c
@@ -2,23 +2,28 @@
 
 
 
-bool width_edit = false, height_edit = false, text_edit = false;
+bool width_edit = false, height_edit = false, text_edit = false, step_edit = false;
 char save_dir[255] ="~/Pictures";
 
 void _confw_draw(void)
 {
   GuiLabel((Rectangle){0,5,150,20}, "#64# Configuration menu");
-  GuiLabel((Rectangle){0,30,100,20}, "#119# width:");
-  GuiLabel((Rectangle){0,60,100,20}, "#119# height:");
-  GuiLabel((Rectangle){0,100,100,20}, "#1# save:");
+  GuiLabel((Rectangle){0,30,100,20}, "#119#  width:");
+  GuiLabel((Rectangle){0,50,100,20}, "#119# height:");
+  GuiLabel((Rectangle){0,70,100,20}, "#119#   step:");
+  GuiLabel((Rectangle){0,100,100,20}, "#1#   save:");
   
   if(GuiValueBox((Rectangle){65,30,130,20}, NULL, &width, 0, 1920, width_edit))
     {
       width_edit = !width_edit;
     }
-  if(GuiValueBox((Rectangle){65,60,130,20}, NULL, &height, 0, 1080, height_edit))
+  if(GuiValueBox((Rectangle){65,50,130,20}, NULL, &height, 0, 1080, height_edit))
     {
       height_edit = !height_edit;
+    }
+  if(GuiValueBox((Rectangle){65,70,130,20}, NULL, &step, 0, 50, step_edit))
+    {
+      step_edit = !step_edit;
     }
   if(GuiTextBox((Rectangle){65,100,130,20}, save_dir, 255, text_edit))
     {
